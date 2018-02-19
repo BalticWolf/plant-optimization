1fb35083cf7344c384b907dbec0afe27b5405191
Timothee Aupetit <timothee.aupetit@gmail.com>
2018-02-19 23:51:12 +0100

    WIP

--- src/main/scala/com/engineering/EnvironmentVariable.scala
+++ src/main/scala/com/engineering/EnvironmentVariable.scala
@@ -2,15 +2,19 @@ package com.engineering
 
 import enumeratum.values.{StringEnum, StringEnumEntry}
 
+import scala.collection.immutable.IndexedSeq
+
 object EnvironmentVariable {
 
   sealed abstract class EnvironmentVariable(val value: String) extends StringEnumEntry
 
-  case object EnvironmentVariable extends StringEnum[EnvironmentVariable] {
+  object EnvironmentVariable extends StringEnum[EnvironmentVariable] {
 
     override def values: IndexedSeq[EnvironmentVariable] = findValues
 
     case object APPLICATION extends EnvironmentVariable("APPLICATION")
+
+    case object OUTPUT_FOLDER_PATH extends EnvironmentVariable("OUTPUT_FOLDER_PATH")
 
   }
 }
--- src/main/scala/com/engineering/MainApplication.scala
+++ src/main/scala/com/engineering/MainApplication.scala
@@ -1,13 +1,15 @@
 package com.engineering
 
 import com.engineering.EnvironmentVariable.EnvironmentVariable.APPLICATION
+import com.engineering.pathmaker.PathMaker
+import com.engineering.optimizer.Optimizer
 
 import scala.util.Properties
 
 object MainApplication extends App {
   val app = Properties.envOrNone(s"$APPLICATION") match {
-    case Some("MAKE_PATH") => Right(new PathMaker)
-//    case Some("OPTIMIZE") => Right(new Optimizer)
+    case Some("PATH_MAKER") => Right(PathMaker)
+    case Some("OPTIMIZER") => Right(Optimizer)
 //    case Some("EVALUATE") => Right(new Evaluation)
     case Some(unknownApplication) => Left(s"APPLICATION '$unknownApplication' is not known")
     case None => Left(s"Environment variable '$APPLICATION' is not defined.")
@@ -15,7 +17,7 @@ object MainApplication extends App {
 
   app match {
     case Right(target) =>
-      target.main()
+      target.main(Array())
     case Left(reason) =>
       println(reason)
   }

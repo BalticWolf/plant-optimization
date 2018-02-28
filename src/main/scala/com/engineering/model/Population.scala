2c89d4776075d10611db60f35e489e28055a6349
Timothee Aupetit <timothee.aupetit@gmail.com>
2018-03-01 00:17:09 +0100

    Implemented Population class

---
+++ src/main/scala/com/engineering/model/Population.scala
@@ -0,0 +1,108 @@
+package com.engineering.model
+
+import scala.util.Random
+
+case class Population(group: List[Individual],
+                      bestGeneration: Int = 0) {
+
+  /**
+    * Test a population evolving through several generations.
+    */
+  def lifeCycle(traffic: Array[Array[Double]], strongest: Individual): Population = {
+    def loop(population: Population, countGeneration: Int): Population = {
+      if (countGeneration < Population.maxGeneration) {
+        val pop = mutate()
+          .evaluate(traffic)
+          .select(strongest, countGeneration)
+        loop(pop, countGeneration + 1)
+      }
+      else population
+    }
+    loop(evaluate(traffic), 0)
+  }
+
+  /**
+    * Evaluate all the Individuals in the population group.
+    * @return evaluated population
+    */
+  def evaluate(traffic: Array[Array[Double]]): Population = {
+    this.copy(
+      group = this.group
+      .filterNot(_.isTested)
+      .map(_.evaluate(traffic))
+    )
+  }
+
+  /**
+    * Make some individuals randomly mutate in the population.
+    * @return population with some mutated individuals
+    */
+  def mutate(): Population = {
+    def loop(before: List[Individual], after: List[Individual]): List[Individual] = before match {
+      case Nil => after
+      case remain =>
+        val p = Population.rand.nextInt(100) / 100F
+        val nextIndividual =
+          if (p < Population.mutationProbability)
+            remain.head.mutate()
+          else
+            remain.head
+
+        loop(remain.tail, after :+ nextIndividual)
+    }
+    this.copy(
+      loop(group.tail, Nil) // exclude the best individual, which is head
+    )
+  }
+
+  /**
+    * Select some of the best individuals.
+    * @return population having some of the best individuals
+    */
+  def select(strongestOfAllTime: Individual, currentGeneration: Int): Population = {
+    val strongestOfGroup = getBestIndividual(strongestOfAllTime :: this.group)
+
+    val bestGeneration = if (strongestOfGroup != strongestOfAllTime) currentGeneration else this.bestGeneration
+
+    def loop(before: List[Individual], after: List[Individual]): List[Individual] = before match {
+      case Nil => after
+      case remain =>
+        val index = Population.rand.nextInt(group.length - 1)
+        val localStrongest = remain(index).whoIsBest(remain.head)
+        loop(remain.tail, after :+ localStrongest)
+    }
+    this.copy(
+      group = loop(this.group.tail, Nil),
+      bestGeneration = bestGeneration
+    )
+  }
+
+  /**
+    * Get the individual having the minimum entropy, among a given list.
+    * @param pop list of individuals
+    * @return best individual regarding its entropy
+    */
+  private def getBestIndividual(pop: List[Individual]): Individual = pop.minBy(_.entropy)
+}
+
+object Population {
+  val rand: Random = new Random()
+
+  private val maxPopulationSize: Int = 30
+  private val mutationProbability: Float = 0.5F
+  private val maxGeneration: Int = 1000
+
+  def apply(): Population = new Population(populate())
+
+  /**
+    * Generate a list of individuals.
+    */
+  private def populate(): List[Individual] = {
+    val group = for {
+      _ <- 0 until maxPopulationSize
+    } yield {
+      Individual()
+    }
+    group.toList
+  }
+}
